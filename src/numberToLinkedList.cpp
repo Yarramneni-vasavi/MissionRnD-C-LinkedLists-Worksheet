0630d8f28c56d35bfb7eaa9432e780740c15f981
Yarramneni-vasavi <Yarramneni-vasavi@missionrnd.com>
2016-03-17 00:32:16 -0700

    Submit Commit

--- src/numberToLinkedList.cpp
+++ src/numberToLinkedList.cpp
@@ -19,6 +19,46 @@ struct node {
 	struct node *next;
 };
 
-struct node * numberToLinkedList(int N) {
-	return NULL;
+struct node * insert_First(struct node *head, int data);
+
+struct node * numberToLinkedList(int N) 
+{
+	struct node *head = NULL;
+	int remainder;
+
+	if (N < 0)
+	{
+		N = -N;
+	}
+	else if (N == 0)
+	{
+		head = insert_First(head, 0);
+		return head;
+	}
+
+	while (N > 0)
+	{
+		remainder = N % 10;
+		head = insert_First(head, remainder);
+		N = N / 10;
+	}
+	return head;
+}
+
+struct node * insert_First(struct node *head, int data)
+{
+	struct node *temp;
+	temp = (struct node*)malloc(sizeof(struct node));
+	temp->num = data;
+	temp->next = NULL;
+	if (head == NULL)
+	{
+		head = temp;
+	}
+	else
+	{
+		temp->next = head;
+		head = temp;
+	}
+	return head;
 }
